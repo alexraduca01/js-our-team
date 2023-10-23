415fffd9e8f780b18ad0f389e0a1a505dee2d20e
alexraduca01 <alex.raduca01@icloud.com>
2023-10-23 14:03:42 +0200

    added scaffolding

---
+++ js/script.js
@@ -0,0 +1,45 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+// Trasformare la stringa foto in una immagine effettiva
+
+const ourTeam = [
+    
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        picture: "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        name: "Angela Carll",
+        role: "Chief Editor",
+        picture: "angela-caroll-chief-editor.jpg",
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        picture: "walter-gordon-office-manager.jpg",
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        picture: "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        picture: "scott-estrada-developer.jpg",
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        picture: "barbara-ramos-graphic-designer.jpg",
+    },
+];
+
+console.log(ourTeam);
